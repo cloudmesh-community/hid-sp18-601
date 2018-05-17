559b97a91d672c37863a090e4976738dac7d101e
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2018-05-17 14:46:25 -0400

    swagger stuff

---
+++ swagger/cloudmesh/subscribers/default_controller.py
@@ -0,0 +1,15 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def sub_post():  # noqa: E501
+    """sub_post
+
+    creates or updates subscriber with name sub_name # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
