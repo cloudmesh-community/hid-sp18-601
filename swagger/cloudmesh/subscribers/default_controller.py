76c6620d48c4b09049cd08071f6931c33ef9bf91
Juliano Gianlupi <julianoferrarigianlupi@gmail.com>
2018-05-18 14:41:32 -0400

    default controler

--- swagger/cloudmesh/subscribers/default_controller.py
+++ swagger/cloudmesh/subscribers/default_controller.py
@@ -3,13 +3,22 @@ import six
 
 from swagger_server import util
 
+from flask import request
 
 def sub_post():  # noqa: E501
     """sub_post
 
-    creates or updates subscriber with name sub_name # noqa: E501
+    creates subscriber with name sub_name # noqa: E501
 
 
     :rtype: None
     """
-    return 'do some magic!'
+    #getting the input
+    subscriberData = raw_input(
+        "Enter the subscriber name and email separated by a comma:")
+    subscriberList = open('./subscriberList.txt','r+')
+    if subscriberDataSplitEmail not in subscriberList:
+        subscriberList.write(subscriberData)
+        subscriberList.write('\n')
+    else:
+        print('Subscriber already present')
